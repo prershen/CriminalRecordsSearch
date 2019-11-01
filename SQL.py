6359adc9ad2b14cfef5fe057d3127a7e094f9014
prershen <perushenoy99@gmail.com>
2019-11-01 20:52:54 +0530

    Made few changes

--- SQL.py
+++ SQL.py
@@ -1,12 +1,12 @@
 import mysql.connector
 from mysql.connector import Error
 
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='Electronics',
-                                         user='pynative',
-                                         password='pynative@#29')
 
+connection = mysql.connector.connect(host='localhost',
+                                         database='dbmslab',
+                                         user='root',
+                                         password='Perushenoy@99')
+try:
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
--- gui.py
+++ gui.py
@@ -7,14 +7,15 @@ from bson import ObjectId
 import gridfs
 import os
 from PIL import Image
-client=MongoClient()
-db=client.dbmslab
+#client=MongoClient()
+#db=client.dbmslab
 
 class Uploader(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
         self.root = Canvas()
         self.root.grid()
+        self.title('Criminal Search')  
         Button(self.root, text='Upload an image', command=self.get_image).grid(padx=50,pady=5)
         Label(text='Click preview picture to upload').grid(pady=5)
 
@@ -22,23 +23,26 @@ class Uploader(Tk):
         self.file_name = askopenfilename(filetypes=[('JPEG FILES', '*.jpg')])
 
         self.image = ImageTk.PhotoImage(Image.open(self.file_name))
-        preview = Toplevel()
-        self.sketch=Label(preview,image=self.image)
+        self.preview = Toplevel()
+        self.sketch=Label(self.preview,image=self.image)
         self.sketch.grid(row=0, column=1, padx=10, pady=10)
-        self.save = Button(preview, command=self.save, text="Save")
+        self.save = Button(self.preview, command=self.saving, text="Save")
         self.save.grid(row=1, column=1, padx=10, pady=10)
-        self.photo= Button(preview, command=self. createPhoto, text="Create Photo")
+        self.photo= Button(self.preview, command=self.createPhoto, text="Create Photo")
         self.photo.grid(row=3, column=1, padx=10, pady=10)
-
+        self.photo= Button(self.preview, command=self.back, text="Back")
+        self.photo.grid(row=5, column=1, padx=10, pady=10)
         self.save.grid()
 
-    def save(self):
-        self.title('Uploading')        
-        gfsPhoto =  gridfs.GridFS(db, "sketch");
-        img=open(self.file_name,'rb')
-        gfsFile = gfsPhoto.put(img,_id='S_1');
+    def saving(self):
+        #gfsPhoto =  gridfs.GridFS(db, "sketch")
+        #img=open(self.file_name,'rb')
+        #gfsFile = gfsPhoto.put(img,_id='S_1')
+        print("Saving")
     def createPhoto(self):
-        self.title('Making Photo')
+        self.text1=Text(self.preview)
+        self.text1.insert(END,"Processing").grid(row=7,column=1,padx=10,pady=10)
+        self.save.grid()
 
 
 app = Uploader()
