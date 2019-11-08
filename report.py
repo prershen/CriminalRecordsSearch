61432d01836a22d6d762c1d9b4ad01b73ae12d3a
pratheeksha22 <51379668+pratheeksha22@users.noreply.github.com>
2019-11-08 10:03:30 +0530

    Update report.py

--- report.py
+++ report.py
@@ -2,12 +2,23 @@ import mysql.connector
 from mysql.connector import Error
 from tkinter import *
 from datetime import *
+from pymongo import MongoClient
+from bson import ObjectId
+import gridfs
+import os,shutil
+from PIL import Image
+import base64
+import codecs
+import face_recognition
+import pprint
 
 class report:
     connection = mysql.connector.connect(host='localhost',
                                          database='dbmslab',
                                          user='root',
                                          password='Perushenoy@99')
+    client=MongoClient();
+    db=client.mugshot;  
     def __init__(self, p_id):
         self.p_id=p_id
         self.mycursor=self.connection.cursor()
@@ -22,6 +33,7 @@ class report:
         self.rlist=[] #robbery
         self.vlist=[] #violence
         self.dlist=[] #drug
+        offence_id=retrieve(p_id)
         """put mongodb statement to get all offence ids of criminal in tlist,rlist,vlist and dlist for p_id
         if len(tlist)!=0:
             sql="SELECT * FROM traffic WHERE offence_id IN("
@@ -109,6 +121,50 @@ class report:
     def offence(self):
         self.root2=Tk()
         self.root2.title('Criminal History')
-
         self.root2.mainloop()
+    
+    
+    def insert(__id,path,offence_id):
+      with open(path, "rb") as imageFile:
+        string = base64.b64encode(imageFile.read())   
+      db.photos.insert_one({"_id":__id,"img":string,"offence_id":offence_id})
+    
+    """insert("P_1","/home/pratz/test_photos/meghna.jpeg",["D_1","V_1"])"""
+
+    def retrieve(__id):    
+      img_dict=db.photos.find_one({"_id":__id},{"img":1,"offence_id":1,"_id":0})
+      string=img_dict["img"] 
+      offence_id=img_dict["offence_id"]
+      fh = open("/home/pratz/retrieved_photos/{}.jpg".format(__id), "wb")
+      fh.write(codecs.decode(string,'base64'))
+      fh.close()
+      return offence_id
+
+    """retrieve("P_1")"""
+
+    def face_match(known_img_path,unknown_img_path):
+      known_image = face_recognition.load_image_file(known_img_path)
+      unknown_image = face_recognition.load_image_file(unknown_img_path)
+      biden_encoding = face_recognition.face_encodings(known_image)[0]
+      unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+      results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+      return results
+  
+    def face_search(unknown_img_path):
+          for itm in db.photos.find({}):
+              retrieve(itm.get('_id'))
+          for filename in os.listdir("/home/pratz/retrieved_photos"):
+            res=face_match("/home/pratz/retrieved_photos/"+filename,unknown_img_path)
+            if res==True:
+               img = Image.open("/home/pratz/retrieved_photos/"+filename)
+               img = img.resize((1024,768))
+               l=filename.split('.')
+               __id=l[0]
+               break
+            else:
+              __id=None        
+          return __id  
+
+    """_id=face_search("/home/pratz/test_photos/meghna.jpeg")
+    print(_id)"""
 found=report("P_1")
